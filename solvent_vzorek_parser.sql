db4e67232a6f85a896d51db33911a0f00c926b70
Petr Suk <petr.suk@clevermaps.cz>
2017-10-11 15:35:00 +0200

    unzip varianta v datech solventu

--- solvent_vzorek_parser.sql
+++ solvent_vzorek_parser.sql
@@ -1,16 +1,33 @@
+-- with ZIP CODE
 SELECT 
        --row_number() OVER (PARTITION BY 1) id,
        src_id,
-       CASE WHEN objekt IS NOT NULL AND adresa IS NOT NULL AND ulice IS NOT NULL THEN  psc_string||','||ulice||' '||objekt||'/'||adresa||' '||obec_orig
-            WHEN objekt IS NOT NULL AND adresa IS NULL AND ulice IS NOT NULL THEN psc_string||','||ulice||' '||objekt||' '||obec_orig
-            WHEN objekt IS NULL AND adresa IS NOT NULL AND ulice IS NOT NULL THEN psc_string||','||ulice||' '||adresa||' '||obec_orig
-            WHEN objekt IS NOT NULL AND adresa IS NOT NULL AND ulice IS NULL THEN psc_string||','||objekt||'/'||adresa||' '||obec_orig
-            WHEN objekt IS NOT NULL AND adresa IS NULL AND ulice IS NULL THEN psc_string||','||objekt||' '||obec_orig
-            WHEN objekt IS NULL AND adresa IS NOT NULL AND ulice IS NULL THEN psc_string||','||adresa||' '||obec_orig
-            WHEN objekt IS NULL AND adresa IS NULL AND ulice IS NOT NULL THEn psc_string||','||ulice||' '||obec_orig
+       CASE WHEN objekt IS NOT NULL AND adresa IS NOT NULL AND ulice IS NOT NULL THEN  psc_string||','||ulice||' '||objekt||'/'||adresa||','||obec_orig
+            WHEN objekt IS NOT NULL AND adresa IS NULL AND ulice IS NOT NULL THEN psc_string||','||ulice||' '||objekt||','||obec_orig
+            WHEN objekt IS NULL AND adresa IS NOT NULL AND ulice IS NOT NULL THEN psc_string||','||ulice||' '||adresa||','||obec_orig
+            WHEN objekt IS NOT NULL AND adresa IS NOT NULL AND ulice IS NULL THEN psc_string||','||objekt||'/'||adresa||','||obec_orig
+            WHEN objekt IS NOT NULL AND adresa IS NULL AND ulice IS NULL THEN psc_string||','||objekt||','||obec_orig
+            WHEN objekt IS NULL AND adresa IS NOT NULL AND ulice IS NULL THEN psc_string||','||adresa||','||obec_orig
+            WHEN objekt IS NULL AND adresa IS NULL AND ulice IS NOT NULL THEN psc_string||','||ulice||','||obec_orig
        END address
    FROM vzorek
      WHERE obec_orig IS NOT NULL AND
            psc_string IS NOT NULL
    ORDER BY random();
 
+-- without ZIP CODE
+SELECT 
+       --row_number() OVER (PARTITION BY 1) id,
+       src_id,
+       CASE WHEN objekt IS NOT NULL AND adresa IS NOT NULL AND ulice IS NOT NULL THEN  ulice||' '||objekt||'/'||adresa||','||obec_orig
+            WHEN objekt IS NOT NULL AND adresa IS NULL AND ulice IS NOT NULL THEN ulice||' '||objekt||','||obec_orig
+            WHEN objekt IS NULL AND adresa IS NOT NULL AND ulice IS NOT NULL THEN ulice||' '||adresa||','||obec_orig
+            WHEN objekt IS NOT NULL AND adresa IS NOT NULL AND ulice IS NULL THEN objekt||'/'||adresa||','||obec_orig
+            WHEN objekt IS NOT NULL AND adresa IS NULL AND ulice IS NULL THEN objekt||','||obec_orig
+            WHEN objekt IS NULL AND adresa IS NOT NULL AND ulice IS NULL THEN adresa||','||obec_orig
+            WHEN objekt IS NULL AND adresa IS NULL AND ulice IS NOT NULL THEN ulice||','||obec_orig
+       END address
+   FROM vzorek
+     WHERE obec_orig IS NOT NULL
+           
+   ORDER BY random();
